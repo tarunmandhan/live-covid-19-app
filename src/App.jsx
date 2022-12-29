f929b1fb989496dd3a08029a9f497129fb4b9e31
Tarun Mandhan <mandhantarun@gmail.com>
2022-12-30 01:59:09 +0530

    add Statewise component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Statewise from "./components/statewiseData/statewise";
+
+const App = () => {
+  return (
+    <>
+      <Statewise />
+    </>
+  );
+};
+
+export default App;
