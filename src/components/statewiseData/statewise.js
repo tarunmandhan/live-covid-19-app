944592eff8f910797dd638909e5cbd84b8d54dc2
Tarun Mandhan <mandhantarun@gmail.com>
2022-12-30 20:45:28 +0530

    change json file link

--- src/components/statewiseData/statewise.js
+++ src/components/statewiseData/statewise.js
@@ -5,7 +5,7 @@ const Statewise = () => {
   const [data, setData] = useState([]);
 
   const getCovidData = async () => {
-    const res = await fetch("https://api.covid19india.org/data.json");
+    const res = await fetch("https://data.covid19india.org/data.json");
     const actualData = await res.json();
     console.log(actualData.statewise);
     setData(actualData.statewise);
@@ -45,7 +45,7 @@ const Statewise = () => {
                     <td>{curElem.deaths}</td>
                     <td>{curElem.active}</td>
                     <td>{curElem.lastUpdatedtime}</td>
-                  </tr> 
+                  </tr>
                 );
               })}
             </tbody>
